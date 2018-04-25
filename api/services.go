5486db2094c19ef95c66db9c9422803a741f9ac2
Joris Melchior <joris.melchior@gmail.com>
2018-04-24 22:46:04 -0400

    Add go:generate for service interfaces. - Add newly generated fakes

--- api/services.go
+++ api/services.go
@@ -2,9 +2,13 @@ package api
 
 import "github.com/jmelchio/vetlab/model"
 
+//go:generate counterfeiter . UserService
+
 // UserService provides the business operations for maintaining users within the application
 type UserService interface {
 }
+
+//go:generate counterfeiter . ReportService
 
 // ReportService provides the business operations for requesting,finding and retrieving
 // diagnostic reports
@@ -12,10 +16,14 @@ type ReportService interface {
 	SubmitDiagnosticRequest(model.DiagnosticRequest) (model.DiagnosticRequest, error)
 }
 
+//go:generate counterfeiter . VetOrgService
+
 // VetOrgService provides the business operations for maintianing veterinary practices
 // within the application
 type VetOrgService interface {
 }
+
+//go:generate counterfeiter . AdminService
 
 // AdminService provides the business operations for all administrative functions
 // within the application
