23c3a7760ff691b9428f80d38b890f288b15a224
Joris Melchior <joris.melchior@gmail.com>
2018-05-01 22:42:38 -0400

    Add more testing - UpdateUser function - DeleteUser function

--- service/user_service.go
+++ service/user_service.go
@@ -12,10 +12,14 @@ type User struct {
 	UserRepo UserRepo
 }
 
+const (
+	MissingContext = "Context is required"
+)
+
 // CreateUser creates a new model.User in the vetlab system
 func (userService User) CreateUser(ctx context.Context, user model.User) (*model.User, error) {
 	if ctx == nil {
-		return nil, errors.New("Context is required")
+		return nil, errors.New(MissingContext)
 	}
 
 	newUser, err := userService.UserRepo.Create(user)
@@ -26,31 +30,43 @@ func (userService User) CreateUser(ctx context.Context, user model.User) (*model
 }
 
 // UpdateUser updates a model.User in the vetlab system
-func (userService User) UpdateUser(context.Context, model.User) (*model.User, error) {
-	return nil, nil
+func (userService User) UpdateUser(ctx context.Context, user model.User) (*model.User, error) {
+	if ctx == nil {
+		return nil, errors.New(MissingContext)
+	}
+
+	newUser, err := userService.UserRepo.Update(user)
+	if err != nil {
+		return nil, err
+	}
+	return newUser, nil
 }
 
 // DeleteUser delets a model.User from the vetlab system
-func (userService User) DeleteUser(context.Context, model.User) error {
-	return nil
+func (userService User) DeleteUser(ctx context.Context, user model.User) error {
+	if ctx == nil {
+		return errors.New(MissingContext)
+	}
+	err := userService.UserRepo.Delete(user)
+	return err
 }
 
 // Login tries to login a user into the vetlab system
-func (userService User) Login(context.Context, string, string) (*model.User, error) {
+func (userService User) Login(ctx context.Context, userName string, password string) (*model.User, error) {
 	return nil, nil
 }
 
 // FindUsersByVetOrg attempts to find users by the veterinary organization
-func (userService User) FindUsersByVetOrg(context.Context, model.VetOrg) ([]model.User, error) {
+func (userService User) FindUsersByVetOrg(ctx context.Context, vetOrg model.VetOrg) ([]model.User, error) {
 	return nil, nil
 }
 
 // FindUsersByName attempts to find users by their name
-func (userService User) FindUsersByName(context.Context, string) ([]model.User, error) {
+func (userService User) FindUsersByName(ctx context.Context, userName string) ([]model.User, error) {
 	return nil, nil
 }
 
 // FindUserByID finds users by their unique ID
-func (userService User) FindUserByID(context.Context, string) (*model.User, error) {
+func (userService User) FindUserByID(ctx context.Context, userID string) (*model.User, error) {
 	return nil, nil
 }
--- service/user_service_test.go
+++ service/user_service_test.go
@@ -26,23 +26,23 @@ var _ = Describe("UserService", func() {
 			newUser     model.User
 			createdUser model.User
 		)
+		BeforeEach(func() {
+
+			newUser = model.User{
+				UserName:     "some-name",
+				FirstName:    "first-name",
+				LastName:     "last-name",
+				Email:        "email@domain.com",
+				PasswordHash: "passord-hash",
+				OrgID:        "org-id",
+				AdminUser:    false,
+			}
+			createdUser = newUser
+			createdUser.UserID = "created-user-id"
+
+			userRepo.CreateReturns(&createdUser, nil)
+		})
 		Context("We have a valid user and 'todo' context", func() {
-			BeforeEach(func() {
-
-				newUser = model.User{
-					UserName:     "some-name",
-					FirstName:    "first-name",
-					LastName:     "last-name",
-					Email:        "email@domain.com",
-					PasswordHash: "passord-hash",
-					OrgID:        "org-id",
-					AdminUser:    false,
-				}
-				createdUser = newUser
-				createdUser.UserID = "created-user-id"
-
-				userRepo.CreateReturns(&createdUser, nil)
-			})
 			It("Returns a user with a new user ID and calls UserRepo.Create", func() {
 				zeUser, err := userService.CreateUser(context.TODO(), newUser)
 				Expect(err).ToNot(HaveOccurred())
@@ -52,9 +52,10 @@ var _ = Describe("UserService", func() {
 		})
 		Context("We have a valid user but no context", func() {
 			It("Returns an error and does not call UserRepo.Create", func() {
-				_, err := userService.CreateUser(nil, newUser)
+				zeUser, err := userService.CreateUser(nil, newUser)
 				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).To(Equal("Context is required"))
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(zeUser).To(BeNil())
 				Expect(userRepo.CreateCallCount()).To(Equal(0))
 			})
 		})
@@ -63,21 +64,111 @@ var _ = Describe("UserService", func() {
 				userRepo.CreateReturns(nil, errors.New("Unable to create user"))
 			})
 			It("Returns an error after calling UserRepo.Create", func() {
-				_, err := userService.CreateUser(context.TODO(), newUser)
+				zeUser, err := userService.CreateUser(context.TODO(), newUser)
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(Equal("Unable to create user"))
+				Expect(zeUser).To(BeNil())
 				Expect(userRepo.CreateCallCount()).To(Equal(1))
 			})
 		})
 	})
 	Describe("Update a user", func() {
-		It("proper test cases and implementation of code", func() {
-			Expect("this").To(Equal("something"))
+		var (
+			user        model.User
+			updatedUser model.User
+		)
+		BeforeEach(func() {
+
+			user = model.User{
+				UserID:       "some-user-id",
+				UserName:     "some-name",
+				FirstName:    "first-name",
+				LastName:     "last-name",
+				Email:        "email@domain.com",
+				PasswordHash: "passord-hash",
+				OrgID:        "org-id",
+				AdminUser:    false,
+			}
+			updatedUser = user
+			updatedUser.UserName = "updated-username"
+			updatedUser.FirstName = "second-first-name"
+
+			userRepo.UpdateReturns(&updatedUser, nil)
+		})
+		Context("We have a valid user and context", func() {
+			It("Returns the updated user and no error", func() {
+				zeUser, err := userService.UpdateUser(context.TODO(), user)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(userRepo.UpdateCallCount()).To(Equal(1))
+				Expect(zeUser.FirstName).To(Equal("second-first-name"))
+			})
+		})
+		Context("We have a valid user but no context", func() {
+			It("Returns and error and no updated user", func() {
+				zeUser, err := userService.UpdateUser(nil, user)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(zeUser).To(BeNil())
+				Expect(userRepo.UpdateCallCount()).To(Equal(0))
+			})
+		})
+		Context("We have a user and Context but repo cannot create user", func() {
+			BeforeEach(func() {
+				userRepo.UpdateReturns(nil, errors.New("Unable to update user"))
+			})
+			It("Returns an error after calling UserRepo.Create", func() {
+				zeUser, err := userService.UpdateUser(context.TODO(), user)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("Unable to update user"))
+				Expect(zeUser).To(BeNil())
+				Expect(userRepo.UpdateCallCount()).To(Equal(1))
+			})
 		})
 	})
 	Describe("Delete a user", func() {
-		It("proper test cases and implementation of code", func() {
-			Expect("this").To(Equal("something"))
+		var (
+			user model.User
+		)
+		BeforeEach(func() {
+
+			user = model.User{
+				UserID:       "some-user-id",
+				UserName:     "some-name",
+				FirstName:    "first-name",
+				LastName:     "last-name",
+				Email:        "email@domain.com",
+				PasswordHash: "passord-hash",
+				OrgID:        "org-id",
+				AdminUser:    false,
+			}
+
+			userRepo.DeleteReturns(nil)
+		})
+		Context("We have a valid user and context", func() {
+			It("Returns no error", func() {
+				err := userService.DeleteUser(context.TODO(), user)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(userRepo.DeleteCallCount()).To(Equal(1))
+			})
+		})
+		Context("We have a valid user but no context", func() {
+			It("Returns and error and no updated user", func() {
+				err := userService.DeleteUser(nil, user)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(userRepo.DeleteCallCount()).To(Equal(0))
+			})
+		})
+		Context("We have a user and Context but repo cannot delete user", func() {
+			BeforeEach(func() {
+				userRepo.DeleteReturns(errors.New("Unable to delete user"))
+			})
+			It("Returns an error after calling UserRepo.Create", func() {
+				err := userService.DeleteUser(context.TODO(), user)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("Unable to delete user"))
+				Expect(userRepo.DeleteCallCount()).To(Equal(1))
+			})
 		})
 	})
 	Describe("Login", func() {
