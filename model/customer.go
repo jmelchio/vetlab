abcbf2b68b91a760110a753bbbdcf2a8ed15ea83
Joris Melchior <joris.melchior@gmail.com>
2018-10-06 21:28:24 -0400

    Add relationships so that Gorm understands them
    - For Customer and VetOrg

--- model/customer.go
+++ model/customer.go
@@ -4,11 +4,13 @@ package model
 // UserName is a pointer because we want to catch if it's null in
 // the database.
 type Customer struct {
-	ID        uint    `json:"id" gorm:"primary_key"`
-	UserName  *string `json:"user_name" gorm:"not null;unique_index"`
-	FirstName string  `json:"first_name,omitempty"`
-	LastName  string  `json:"last_name,omitempty"`
-	Email     string  `json:"email,omitempty"`
-	Password  string  `json:"password,omitempty"`
-	VetOrgID  uint    `json:"vet_org_id"`
+	ID                 uint                `json:"id" gorm:"primary_key"`
+	UserName           *string             `json:"user_name" gorm:"not null;unique_index"`
+	FirstName          string              `json:"first_name,omitempty"`
+	LastName           string              `json:"last_name,omitempty"`
+	Email              string              `json:"email,omitempty"`
+	Password           string              `json:"password,omitempty"`
+	VetOrgID           uint                `json:"vet_org_id"`
+	DiagnosticReports  []DiagnosticReport  `json:"diagnostic_reports,omitempty"`
+	DiagnosticRequests []DiagnosticRequest `json:"diagnostic_requests,omitempty"`
 }
--- model/vetorg.go
+++ model/vetorg.go
@@ -2,15 +2,16 @@ package model
 
 // VetOrg declares the structure for veterinary practice data
 type VetOrg struct {
-	ID          uint    `json:"id" gorm:"primary_key"`
-	OrgName     *string `json:"org_name" gorm:"unique_index;not null"`
-	Street      string  `json:"street,omitempty"`
-	HouseNumber string  `json:"house_number,omitempty"`
-	City        string  `json:"city,omitempty"`
-	Province    string  `json:"province,omitempty"`
-	Country     string  `json:"country,omitempty"`
-	PostalCode  string  `json:"postal_code,omitempty"`
-	Email       string  `json:"email,omitempty"`
-	Phone       string  `json:"phone,omitempty"`
-	Fax         string  `json:"fax,omitempty"`
+	ID          uint       `json:"id" gorm:"primary_key"`
+	OrgName     *string    `json:"org_name" gorm:"unique_index;not null"`
+	Street      string     `json:"street,omitempty"`
+	HouseNumber string     `json:"house_number,omitempty"`
+	City        string     `json:"city,omitempty"`
+	Province    string     `json:"province,omitempty"`
+	Country     string     `json:"country,omitempty"`
+	PostalCode  string     `json:"postal_code,omitempty"`
+	Email       string     `json:"email,omitempty"`
+	Phone       string     `json:"phone,omitempty"`
+	Fax         string     `json:"fax,omitempty"`
+	Employees   []Customer `json:"employees,omitempty"`
 }
