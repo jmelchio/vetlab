c0e8e3058bee24fb4a9cf4da1d4ab914f9a9c32e
Joris Melchior <joris.melchior@gmail.com>
2018-10-06 23:34:29 -0400

    Improve the automigration of tables
    - Check for errors on AutoMigrate
    - Check for existence after AutoMigrate
    - Log success and errors

--- vetlabcmd/command.go
+++ vetlabcmd/command.go
@@ -3,6 +3,7 @@ package vetlabcmd
 import (
 	"log"
 	"net/http"
+	"reflect"
 
 	"github.com/jinzhu/gorm"
 	"github.com/jmelchio/vetlab/api"
@@ -18,16 +19,16 @@ var (
 )
 
 func Run() {
-	database, err = gorm.Open("postgres", "host=localhost user=postgres sslmode=disable")
+	database, err = gorm.Open("postgres", "host=localhost user=postgres dbname=vetlab sslmode=disable")
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %s", err.Error())
 	}
 
-	database.AutoMigrate(&model.VetOrg{})
-	database.AutoMigrate(&model.DiagnosticReport{})
-	database.AutoMigrate(&model.DiagnosticRequest{})
-	database.AutoMigrate(&model.User{})
-	database.AutoMigrate(&model.Customer{})
+	autoMigrateDB(&model.DiagnosticReport{})
+	autoMigrateDB(&model.DiagnosticRequest{})
+	autoMigrateDB(&model.Customer{})
+	autoMigrateDB(&model.VetOrg{})
+	autoMigrateDB(&model.User{})
 
 	userRepo := repository.SQLUserRepo{Database: database}
 	userService := service.User{UserRepo: userRepo}
@@ -37,5 +38,29 @@ func Run() {
 	}
 	http.Handle("/", userHandler)
 
+	log.Println("Starting listner on port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func autoMigrateDB(table interface{}) {
+	if err := database.AutoMigrate(table).Error; err != nil {
+		dbCreateFatal(table, err)
+	}
+	if !database.HasTable(table) {
+		dbMissing(reflect.TypeOf(table).String())
+	} else {
+		dbMigrated(reflect.TypeOf(table).String())
+	}
+}
+
+func dbCreateFatal(table interface{}, err error) {
+	log.Fatalf("Unable to create table: %s, %s", reflect.TypeOf(table).String(), err.Error())
+}
+
+func dbMissing(table string) {
+	log.Fatalf("DB table missing: %s", table)
+}
+
+func dbMigrated(table string) {
+	log.Printf("DB table migrated: %s", table)
 }
