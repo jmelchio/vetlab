51ceb64427a6eab9caab033631467bfc80d07e8f
Joris Melchior <joris.melchior@gmail.com>
2018-10-07 15:49:25 -0400

    Start of customer service
    - More refinement and testing required

---
+++ service/customer_service.go
@@ -0,0 +1,117 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/jmelchio/vetlab/model"
+)
+
+// Customer implements the api.CustomerService interface
+type Customer struct {
+	CustomerRepo CustomerRepo
+}
+
+// CreateCustomer creates a new model.Customer in the vetlab system
+// The assumption is that for a new customer, the password has not been encrypted
+// up until this point so this is done before storing it in the repository.
+func (customerService Customer) CreateCustomer(ctx context.Context, customer model.Customer) (*model.Customer, error) {
+	if ctx == nil {
+		return nil, errors.New(MissingContext)
+	}
+
+	pwdHash, err := hashAndSalt(customer.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	customer.Password = *pwdHash
+	err = customerService.CustomerRepo.Create(&customer)
+	if err != nil {
+		return nil, err
+	}
+	return &customer, nil
+}
+
+// UpdateCustomer updates a model.Customer in the vetlab system
+func (customerService Customer) UpdateCustomer(ctx context.Context, customer model.Customer) (*model.Customer, error) {
+	if ctx == nil {
+		return nil, errors.New(MissingContext)
+	}
+
+	err := customerService.CustomerRepo.Update(&customer)
+	if err != nil {
+		return nil, err
+	}
+	return &customer, nil
+}
+
+// DeleteCustomer delets a model.Customer from the vetlab system
+func (customerService Customer) DeleteCustomer(ctx context.Context, customer model.Customer) error {
+	if ctx == nil {
+		return errors.New(MissingContext)
+	}
+	err := customerService.CustomerRepo.Delete(&customer)
+	return err
+}
+
+// UpdatePassword allows for the change of a customer password
+// We do not check the old password when changing to a new one since we assume the customer
+// has been authenticated
+func (customerService Customer) UpdatePassword(ctx context.Context, customer model.Customer, password string) (*model.Customer, error) {
+	if ctx == nil {
+		return nil, errors.New(MissingContext)
+	}
+
+	pwdHash, err := hashAndSalt(password)
+	if err != nil {
+		return nil, fmt.Errorf(HashingFailed, err.Error())
+	}
+
+	customer.Password = *pwdHash
+	err = customerService.CustomerRepo.Update(&customer)
+	if err != nil {
+		return nil, err
+	}
+
+	return &customer, nil
+}
+
+// Login tries to login a customer into the vetlab system
+func (customerService Customer) Login(ctx context.Context, userName string, password string) (*model.Customer, error) {
+	if ctx == nil {
+		return nil, errors.New(MissingContext)
+	}
+
+	if userName == "" || password == "" {
+		return nil, errors.New(UserOrPasswordFail)
+	}
+
+	customer, err := customerService.CustomerRepo.GetByUserName(userName)
+	if err != nil {
+		return nil, errors.New(UserOrPasswordFail)
+	}
+
+	if !equalPasswords(customer.Password, password) {
+		return nil, errors.New(UserOrPasswordFail)
+	}
+
+	return customer, nil
+}
+
+// FindCustomerByUserName attempts to find customers by their userName
+func (customerService Customer) FindCustomerByUserName(ctx context.Context, userName string) (*model.Customer, error) {
+	if ctx == nil {
+		return nil, errors.New(MissingContext)
+	}
+	return customerService.CustomerRepo.GetByUserName(userName)
+}
+
+// FindCustomerByID finds customers by their unique ID
+func (customerService Customer) FindCustomerByID(ctx context.Context, customerID uint) (*model.Customer, error) {
+	if ctx == nil {
+		return nil, errors.New(MissingContext)
+	}
+	return customerService.CustomerRepo.GetByID(customerID)
+}
---
+++ service/customer_service_test.go
@@ -0,0 +1,501 @@
+package service_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/jmelchio/vetlab/model"
+	. "github.com/jmelchio/vetlab/service"
+	"github.com/jmelchio/vetlab/service/servicefakes"
+	"golang.org/x/crypto/bcrypt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CustomerService", func() {
+
+	var (
+		customerService Customer
+		customerRepo    *servicefakes.FakeCustomerRepo
+		customer        model.Customer
+		userName        string
+		firstName       string
+		lastName        string
+		email           string
+		password        string
+	)
+
+	BeforeEach(func() {
+		customerRepo = new(servicefakes.FakeCustomerRepo)
+		customerService = Customer{CustomerRepo: customerRepo}
+
+		userName = "some-name"
+		firstName = "first-name"
+		lastName = "last-name"
+		email = "email@domain.com"
+		password = "password-hash"
+
+		customer = model.Customer{
+			UserName:  &userName,
+			FirstName: firstName,
+			LastName:  lastName,
+			Email:     email,
+			Password:  password,
+		}
+	})
+
+	Describe("Create a customer", func() {
+
+		BeforeEach(func() {
+			customerRepo.CreateReturns(nil)
+		})
+
+		Context("We have a valid customer and 'todo' context", func() {
+
+			It("Returns a customer with a new customer ID and calls CustomerRepo.Create", func() {
+				zeCustomer, err := customerService.CreateCustomer(context.TODO(), customer)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(zeCustomer).NotTo(BeNil())
+				Expect(customerRepo.CreateCallCount()).To(Equal(1))
+			})
+		})
+
+		Context("Customer password is too short but we have a valid context", func() {
+
+			BeforeEach(func() {
+				customer.Password = "uhSeven"
+			})
+
+			It("Returns an error and does not call CustomerRepo.Create", func() {
+				zeCustomer, err := customerService.CreateCustomer(context.TODO(), customer)
+				Expect(err).To(HaveOccurred())
+				Expect(zeCustomer).To(BeNil())
+				Expect(err.Error()).To(Equal(PasswordTooShort))
+				Expect(customerRepo.CreateCallCount()).To(Equal(0))
+			})
+		})
+
+		Context("We have a valid customer but no context", func() {
+
+			It("Returns an error and does not call CustomerRepo.Create", func() {
+				zeCustomer, err := customerService.CreateCustomer(nil, customer)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(zeCustomer).To(BeNil())
+				Expect(customerRepo.CreateCallCount()).To(Equal(0))
+			})
+		})
+
+		Context("We have a customer and Context but repo cannot create customer", func() {
+
+			BeforeEach(func() {
+				customerRepo.CreateReturns(errors.New("Unable to create customer"))
+			})
+
+			It("Returns an error after calling CustomerRepo.Create", func() {
+				zeCustomer, err := customerService.CreateCustomer(context.TODO(), customer)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("Unable to create customer"))
+				Expect(zeCustomer).To(BeNil())
+				Expect(customerRepo.CreateCallCount()).To(Equal(1))
+			})
+		})
+	})
+
+	Describe("Update a customer", func() {
+
+		BeforeEach(func() {
+			customerRepo.UpdateReturns(nil)
+		})
+
+		Context("We have a valid customer and context", func() {
+
+			It("Returns the updated customer and no error", func() {
+				zeCustomer, err := customerService.UpdateCustomer(context.TODO(), customer)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(customerRepo.UpdateCallCount()).To(Equal(1))
+				Expect(zeCustomer).NotTo(BeNil())
+			})
+		})
+
+		Context("We have a valid customer but no context", func() {
+
+			It("Returns and error and no updated customer", func() {
+				zeCustomer, err := customerService.UpdateCustomer(nil, customer)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(zeCustomer).To(BeNil())
+				Expect(customerRepo.UpdateCallCount()).To(Equal(0))
+			})
+		})
+
+		Context("We have a customer and Context but repo cannot create customer", func() {
+
+			BeforeEach(func() {
+				customerRepo.UpdateReturns(errors.New("Unable to update customer"))
+			})
+
+			It("Returns an error after calling CustomerRepo.Create", func() {
+				zeCustomer, err := customerService.UpdateCustomer(context.TODO(), customer)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("Unable to update customer"))
+				Expect(zeCustomer).To(BeNil())
+				Expect(customerRepo.UpdateCallCount()).To(Equal(1))
+			})
+		})
+	})
+
+	Describe("Delete a customer", func() {
+
+		BeforeEach(func() {
+			customerRepo.DeleteReturns(nil)
+		})
+
+		Context("We have a valid customer and context", func() {
+
+			It("Returns no error", func() {
+				err := customerService.DeleteCustomer(context.TODO(), customer)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(customerRepo.DeleteCallCount()).To(Equal(1))
+			})
+		})
+
+		Context("We have a valid customer but no context", func() {
+
+			It("Returns and error and no updated customer", func() {
+				err := customerService.DeleteCustomer(nil, customer)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(customerRepo.DeleteCallCount()).To(Equal(0))
+			})
+		})
+
+		Context("We have a customer and Context but repo cannot delete customer", func() {
+
+			BeforeEach(func() {
+				customerRepo.DeleteReturns(errors.New("Unable to delete customer"))
+			})
+
+			It("Returns an error after calling CustomerRepo.Create", func() {
+				err := customerService.DeleteCustomer(context.TODO(), customer)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("Unable to delete customer"))
+				Expect(customerRepo.DeleteCallCount()).To(Equal(1))
+			})
+		})
+	})
+
+	Describe("UpdatePassword", func() {
+
+		var (
+			newPwd string
+		)
+
+		BeforeEach(func() {
+			newPwd = "some-magic-password"
+			customerRepo.UpdateReturns(nil)
+		})
+
+		Context("We have a valid customer, password and context", func() {
+
+			It("It returns no error", func() {
+				updatedCustomer, err := customerService.UpdatePassword(context.TODO(), customer, newPwd)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(customerRepo.UpdateCallCount()).To(Equal(1))
+				err = bcrypt.CompareHashAndPassword([]byte(updatedCustomer.Password), []byte(newPwd))
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("We have a valid customer, password but no context", func() {
+
+			It("Returns an error", func() {
+				updatedCustomer, err := customerService.UpdatePassword(nil, customer, newPwd)
+				Expect(err).To(HaveOccurred())
+				Expect(updatedCustomer).To(BeNil())
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(customerRepo.UpdateCallCount()).To(Equal(0))
+			})
+		})
+
+		Context("We have a valid customer, password and context but update fails", func() {
+
+			BeforeEach(func() {
+				customerRepo.UpdateReturns(errors.New("Unable to update customer"))
+			})
+
+			It("returns an error", func() {
+				updatedCustomer, err := customerService.UpdatePassword(context.TODO(), customer, newPwd)
+				Expect(err).To(HaveOccurred())
+				Expect(updatedCustomer).To(BeNil())
+				Expect(err.Error()).To(Equal("Unable to update customer"))
+				Expect(customerRepo.UpdateCallCount()).To(Equal(1))
+			})
+		})
+
+		Context("We have a valid customer, a short password and context", func() {
+
+			BeforeEach(func() {
+				newPwd = "uhseven"
+				customerRepo.UpdateReturns(errors.New("Unable to update customer"))
+			})
+
+			It("returns an error", func() {
+				updatedCustomer, err := customerService.UpdatePassword(context.TODO(), customer, newPwd)
+				Expect(err).To(HaveOccurred())
+				Expect(updatedCustomer).To(BeNil())
+				Expect(err.Error()).To(Equal(fmt.Sprintf(HashingFailed, PasswordTooShort)))
+				Expect(customerRepo.UpdateCallCount()).To(Equal(0))
+			})
+		})
+	})
+
+	Describe("Login", func() {
+
+		var (
+			luserName      string
+			password       string
+			sampleCustomer model.Customer
+		)
+
+		BeforeEach(func() {
+			luserName = "user"
+			password = "somepwdd"
+		})
+
+		Context("Customername, password and context are all correct", func() {
+
+			BeforeEach(func() {
+				password, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+				stringPassword := string(password)
+				sampleCustomer = model.Customer{
+					ID:       uint(12345),
+					UserName: &luserName,
+					Password: stringPassword,
+				}
+				customerRepo.GetByUserNameReturns(&sampleCustomer, nil)
+			})
+
+			It("Returns an authenticated customer", func() {
+				customer, err := customerService.Login(context.TODO(), luserName, password)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(customer).NotTo(BeNil())
+				Expect(customer.ID).To(Equal(sampleCustomer.ID))
+				Expect(customerRepo.GetByUserNameCallCount()).To(Equal(1))
+			})
+		})
+
+		Context("No context provided", func() {
+
+			It("Returns an error", func() {
+				customer, err := customerService.Login(nil, luserName, password)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(customer).To(BeNil())
+				Expect(customerRepo.GetByUserNameCallCount()).To(Equal(0))
+			})
+		})
+
+		Context("Customername or password are not provided", func() {
+
+			It("Returns an error", func() {
+				customer, err := customerService.Login(context.TODO(), "", "")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(UserOrPasswordFail))
+				Expect(customer).To(BeNil())
+				Expect(customerRepo.GetByUserNameCallCount()).To(Equal(0))
+			})
+		})
+
+		Context("Wrong password is provided", func() {
+
+			BeforeEach(func() {
+				password, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+				stringPassword := string(password)
+				sampleCustomer = model.Customer{
+					ID:       uint(12345),
+					UserName: &luserName,
+					Password: stringPassword,
+				}
+				customerRepo.GetByUserNameReturns(&sampleCustomer, nil)
+			})
+
+			It("Returns an error", func() {
+				customer, err := customerService.Login(context.TODO(), userName, "some-other-pwd")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(UserOrPasswordFail))
+				Expect(customer).To(BeNil())
+				Expect(customerRepo.GetByUserNameCallCount()).To(Equal(1))
+			})
+		})
+
+		Context("Underlying repository returns an error", func() {
+
+			BeforeEach(func() {
+				customerRepo.GetByUserNameReturns(nil, errors.New("some-error"))
+			})
+
+			It("Returns an error", func() {
+				customer, err := customerService.Login(context.TODO(), userName, password)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(UserOrPasswordFail))
+				Expect(customer).To(BeNil())
+				Expect(customerRepo.GetByUserNameCallCount()).To(Equal(1))
+			})
+		})
+	})
+
+	Describe("Find a customer by username", func() {
+
+		var (
+			customer   model.Customer
+			userName   string
+			fUserName  string
+			fFirstName string
+			fLastName  string
+		)
+
+		BeforeEach(func() {
+			fUserName = "some-user-name"
+			fFirstName = "john"
+			fLastName = "doe"
+
+			customer = model.Customer{
+				UserName:  &fUserName,
+				FirstName: fFirstName,
+				LastName:  fLastName,
+			}
+
+			userName = "some-user-name"
+
+			customerRepo.GetByUserNameReturns(&customer, nil)
+		})
+
+		Context("Context is correct and username exists", func() {
+
+			It("Returns a customer with the given username", func() {
+				foundCustomer, err := customerService.FindCustomerByUserName(context.TODO(), userName)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(customer).NotTo(BeNil())
+				Expect(customerRepo.GetByUserNameCallCount()).To(Equal(1))
+				Expect(*foundCustomer.UserName).To(Equal(userName))
+			})
+		})
+
+		Context("UserName provided but Context missing", func() {
+
+			It("Returns and error", func() {
+				foundCustomer, err := customerService.FindCustomerByUserName(nil, userName)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(foundCustomer).To(BeNil())
+			})
+		})
+
+		Context("Context and customerName provided but customer is not found", func() {
+
+			BeforeEach(func() {
+				customerRepo.GetByUserNameReturns(nil, nil)
+			})
+
+			It("Returns no error and no customer", func() {
+				foundCustomer, err := customerService.FindCustomerByUserName(context.TODO(), "john doe")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(foundCustomer).To(BeNil())
+			})
+		})
+
+		Context("Context and userName are correct but Repo returns error", func() {
+
+			BeforeEach(func() {
+				customerRepo.GetByUserNameReturns(nil, errors.New("BAM!"))
+			})
+
+			It("Returns an error", func() {
+				foundCustomer, err := customerService.FindCustomerByUserName(context.TODO(), userName)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("BAM!"))
+				Expect(foundCustomer).To(BeNil())
+			})
+		})
+	})
+
+	Describe("Find a customer by ID", func() {
+
+		var (
+			customer   model.Customer
+			customerID uint
+			fUserName  string
+			fFirstName string
+			fLastName  string
+		)
+
+		BeforeEach(func() {
+			fUserName = "some-user-name"
+			fFirstName = "john"
+			fLastName = "doe"
+
+			customer = model.Customer{
+				ID:        uint(12345),
+				UserName:  &fUserName,
+				FirstName: fFirstName,
+				LastName:  fLastName,
+			}
+
+			customerID = 12345
+
+			customerRepo.GetByIDReturns(&customer, nil)
+		})
+
+		Context("Context is correct and customerID exists", func() {
+
+			It("Returns a customer with the given customerID", func() {
+				foundCustomer, err := customerService.FindCustomerByID(context.TODO(), customerID)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(customer).NotTo(BeNil())
+				Expect(customerRepo.GetByIDCallCount()).To(Equal(1))
+				Expect(foundCustomer.ID).To(Equal(customerID))
+			})
+		})
+
+		Context("customerName provided but Context missing", func() {
+
+			It("Returns and error", func() {
+				foundCustomer, err := customerService.FindCustomerByID(nil, customerID)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal(MissingContext))
+				Expect(foundCustomer).To(BeNil())
+			})
+
+		})
+
+		Context("Context and customerID provided but customer is not found", func() {
+
+			BeforeEach(func() {
+				customerRepo.GetByIDReturns(nil, nil)
+			})
+
+			It("Returns no error and no customer", func() {
+				foundCustomer, err := customerService.FindCustomerByID(context.TODO(), 12345)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(foundCustomer).To(BeNil())
+			})
+		})
+
+		Context("Context and customerID are correct but Repo returns error", func() {
+
+			BeforeEach(func() {
+				customerRepo.GetByIDReturns(nil, errors.New("BAM!"))
+			})
+
+			It("Returns an error", func() {
+				foundCustomer, err := customerService.FindCustomerByID(context.TODO(), customerID)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("BAM!"))
+				Expect(foundCustomer).To(BeNil())
+			})
+		})
+	})
+})
