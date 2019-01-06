20f7fe315331898f34880eea129a7cde44e11f26
Joris Melchior <joris.melchior@gmail.com>
2019-01-06 18:18:46 -0500

    Added implementation for submit diagnostic request

--- api/diagnostic_request_handler.go
+++ api/diagnostic_request_handler.go
@@ -1,8 +1,13 @@
 package api
 
 import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 
+	"github.com/jmelchio/vetlab/model"
 	"github.com/tedsuo/rata"
 )
 
@@ -12,7 +17,8 @@ type DiagnosticRequestServer struct {
 }
 
 const (
-	SubmitDiagnosticRequest = "submit_diagnostic_request"
+	SubmitDiagnosticRequest         = "submit_diagnostic_request"
+	UnableToSubmitDiagnosticRequest = "Unable to submit diagnostic request"
 )
 
 // DiagnosticRequestRoutes are the REST endpoint routes for the diagnostic requests REST interface
@@ -33,4 +39,33 @@ func NewDiagnosticRequestHandler(diagnosticRequestService DiagnosticRequestServi
 
 // SubmitDiagnosticRequest is the REST function that allows for the creation of a report request
 func (diagnosticRequestServer *DiagnosticRequestServer) SubmitDiagnosticRequest(writer http.ResponseWriter, request *http.Request) {
+	if request.Body == nil {
+		http.Error(writer, EmptyBody, http.StatusBadRequest)
+		return
+	}
+
+	requestBody, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var diagnosticRequest model.DiagnosticRequest
+	err = json.Unmarshal(requestBody, &diagnosticRequest)
+	if err != nil {
+		http.Error(writer, InvalidBody, http.StatusBadRequest)
+		return
+	}
+
+	newDiagnosticRequest, err := diagnosticRequestServer.DiagnosticRequestService.SubmitDiagnosticRequest(context.TODO(), diagnosticRequest)
+	if err != nil {
+		http.Error(writer, UnableToSubmitDiagnosticRequest, http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(writer).Encode(newDiagnosticRequest); err != nil {
+		log.Printf("Problem encoding new diagnostic request: %s", err.Error())
+	}
 }
