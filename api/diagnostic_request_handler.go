834a2c068ecc0a64a0cd44742f1440e505608b68
Joris Melchior <joris.melchior@gmail.com>
2019-01-08 22:23:15 -0500

    Add Routes for query requests on diagnostic requests
    Add Handler definitions for above requests
    Add stub for handler function

--- api/diagnostic_request_handler.go
+++ api/diagnostic_request_handler.go
@@ -17,13 +17,23 @@ type DiagnosticRequestServer struct {
 }
 
 const (
-	SubmitDiagnosticRequest         = "submit_diagnostic_request"
-	UnableToSubmitDiagnosticRequest = "Unable to submit diagnostic request"
+	SubmitDiagnosticRequest                  = "submit_diagnostic_request"
+	UnableToSubmitDiagnosticRequest          = "Unable to submit diagnostic request"
+	DiagnosticRequestByID                    = "diagnostic_request_by_id"
+	DiagnosticRequestsByVetOrgID             = "diagnotistic_requests_by_vetorg_id"
+	DiagnosticRequestsByUserID               = "diagnotistic_requests_by_user_id"
+	DiagnosticRequestsByCustomerID           = "diagnotistic_requests_by_customer_id"
+	DiagnosticRequestsByVetOrgIDAndDateRange = "diagnostic_requests_by_vetorg_id_and_date_range"
 )
 
 // DiagnosticRequestRoutes are the REST endpoint routes for the diagnostic requests REST interface
 var DiagnosticRequestRoutes = rata.Routes{
 	{Path: "/diagnosticrequest", Method: rata.POST, Name: SubmitDiagnosticRequest},
+	{Path: "/diagnosticrequest/:requestID", Method: rata.GET, Name: DiagnosticRequestByID},
+	{Path: "/diagnosticrequest/vetorg/:vetorgID", Method: rata.GET, Name: DiagnosticRequestsByVetOrgID},
+	{Path: "/diagnosticrequest/vetorg/:vetorgID/start/:startDate/end/:endDate", Method: rata.GET, Name: DiagnosticRequestsByVetOrgIDAndDateRange},
+	{Path: "/diagnosticrequest/user/:userID", Method: rata.GET, Name: DiagnosticRequestsByUserID},
+	{Path: "/diagnosticrequest/customer/:customerID", Method: rata.GET, Name: DiagnosticRequestsByCustomerID},
 }
 
 // NewDiagnosticRequestHandler provides the factory function to create the REST interface for report requests
@@ -31,7 +41,12 @@ func NewDiagnosticRequestHandler(diagnosticRequestService DiagnosticRequestServi
 	diagnosticRequestServer := &DiagnosticRequestServer{DiagnosticRequestService: diagnosticRequestService}
 
 	handlers := rata.Handlers{
-		SubmitDiagnosticRequest: http.HandlerFunc(diagnosticRequestServer.SubmitDiagnosticRequest),
+		SubmitDiagnosticRequest:                  http.HandlerFunc(diagnosticRequestServer.SubmitDiagnosticRequest),
+		DiagnosticRequestByID:                    http.HandlerFunc(diagnosticRequestServer.FindDiagnotisticRequests),
+		DiagnosticRequestsByVetOrgID:             http.HandlerFunc(diagnosticRequestServer.FindDiagnotisticRequests),
+		DiagnosticRequestsByUserID:               http.HandlerFunc(diagnosticRequestServer.FindDiagnotisticRequests),
+		DiagnosticRequestsByCustomerID:           http.HandlerFunc(diagnosticRequestServer.FindDiagnotisticRequests),
+		DiagnosticRequestsByVetOrgIDAndDateRange: http.HandlerFunc(diagnosticRequestServer.FindDiagnotisticRequests),
 	}
 
 	return rata.NewRouter(DiagnosticRequestRoutes, handlers)
@@ -68,4 +83,8 @@ func (diagnosticRequestServer *DiagnosticRequestServer) SubmitDiagnosticRequest(
 	if err := json.NewEncoder(writer).Encode(newDiagnosticRequest); err != nil {
 		log.Printf("Problem encoding new diagnostic request: %s", err.Error())
 	}
+}
+
+// FindDiagnotisticRequests is a handler that handles all types of find requests for diagnostic requests
+func (diagnosticRequestServer *DiagnosticRequestServer) FindDiagnotisticRequests(writer http.ResponseWriter, request *http.Request) {
 }
