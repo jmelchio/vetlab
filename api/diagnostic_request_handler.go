b417606649de127ac732f9c27785926ab42eba3c
Joris Melchior <joris.melchior@gmail.com>
2019-01-10 22:38:30 -0500

    Write implementation for fetching diagnostic requests by ID
    Not all cases covered yet

--- api/diagnostic_request_handler.go
+++ api/diagnostic_request_handler.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/jmelchio/vetlab/model"
 	"github.com/tedsuo/rata"
@@ -24,6 +25,8 @@ const (
 	DiagnosticRequestsByUserID               = "diagnotistic_requests_by_user_id"
 	DiagnosticRequestsByCustomerID           = "diagnotistic_requests_by_customer_id"
 	DiagnosticRequestsByVetOrgIDAndDateRange = "diagnostic_requests_by_vetorg_id_and_date_range"
+	UnableToParseVars                        = "Unable to parse request variable(s)"
+	ErrorFetchingDiagnosticRequests          = "Error occurred attempting to retrieve diagnostic request(s)"
 )
 
 // DiagnosticRequestRoutes are the REST endpoint routes for the diagnostic requests REST interface
@@ -87,6 +90,24 @@ func (diagnosticRequestServer *DiagnosticRequestServer) SubmitDiagnosticRequest(
 
 // FindDiagnotisticRequest is a handler that handles searches for diagstic requests by ID
 func (diagnosticRequestServer *DiagnosticRequestServer) FindDiagnotisticRequest(writer http.ResponseWriter, request *http.Request) {
+	requestID, err := strconv.ParseUint(rata.Param(request, "request_id"), 10, 32)
+	if err != nil {
+		http.Error(writer, UnableToParseVars, http.StatusBadRequest)
+		return
+	}
+
+	diagnosticRequest, err := diagnosticRequestServer.DiagnosticRequestService.FindRequestByID(context.TODO(), uint(requestID))
+	if err != nil {
+		http.Error(writer, ErrorFetchingDiagnosticRequests, http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(writer).Encode(diagnosticRequest); err != nil {
+		log.Printf("Problem encoding returned diagnostic request(s): %s", err.Error())
+		return
+	}
 }
 
 // FindDiagnotisticRequestByVetOrg is a handler that handles searches for diagnostic requests by VetOrg
