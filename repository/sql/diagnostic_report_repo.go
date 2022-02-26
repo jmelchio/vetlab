38bba6fd7346afa643f91cb637e8e6e34f802475
Joris Melchior <joris.melchior@gmail.com>
2022-02-26 14:43:16 -0500

    Implement creation of request and report
    - don't use panic for incomplete implementation but return an error
      indicating that code is not implemented yet

--- repository/sql/diagnostic_report_repo.go
+++ repository/sql/diagnostic_report_repo.go
@@ -1,6 +1,8 @@
 package sql
 
 import (
+	"errors"
+
 	"github.com/jmelchio/vetlab/model"
 	"gorm.io/gorm"
 )
@@ -9,37 +11,38 @@ type DiagnosticReportRepo struct {
 	Database *gorm.DB
 }
 
+// Create creates a persistent DiagnosticReport row in the sql datastore
+// When a given DiagnosticReport already has a non-zero ID an error will be returned
 func (diagnosticReportRepo *DiagnosticReportRepo) Create(diagnosticReport *model.DiagnosticReport) error {
-	//TODO implement me
-	panic("implement me")
+	if diagnosticReport.ID == 0 {
+		if err := diagnosticReportRepo.Database.Create(diagnosticReport).Error; err != nil {
+			return err
+		}
+		return nil
+	}
+	return errors.New("record already in database")
 }
 
 func (diagnosticReportRepo *DiagnosticReportRepo) Update(diagnosticReport *model.DiagnosticReport) error {
-	//TODO implement me
-	panic("implement me")
+	return errors.New("not implemented")
 }
 
 func (diagnosticReportRepo *DiagnosticReportRepo) Delete(name string) error {
-	//TODO implement me
-	panic("implement me")
+	return errors.New("not implemented")
 }
 
 func (diagnosticReportRepo *DiagnosticReportRepo) GetByID(diagnosticReportID uint) (*model.DiagnosticReport, error) {
-	//TODO implement me
-	panic("implement me")
+	return nil, errors.New("not implemented")
 }
 
 func (diagnosticReportRepo *DiagnosticReportRepo) GetByVetOrgID(vetOrgID uint) ([]model.DiagnosticReport, error) {
-	//TODO implement me
-	panic("implement me")
+	return nil, errors.New("not implemented")
 }
 
 func (diagnosticReportRepo *DiagnosticReportRepo) GetByUserID(userID uint) ([]model.DiagnosticRequest, error) {
-	//TODO implement me
-	panic("implement me")
+	return nil, errors.New("not implemented")
 }
 
 func (diagnosticReportRepo *DiagnosticReportRepo) GetByCustomerID(customerID uint) ([]model.DiagnosticRequest, error) {
-	//TODO implement me
-	panic("implement me")
+	return nil, errors.New("not implemented")
 }
--- repository/sql/diagnostic_request_repo.go
+++ repository/sql/diagnostic_request_repo.go
@@ -1,6 +1,8 @@
 package sql
 
 import (
+	"errors"
+
 	"github.com/jmelchio/vetlab/model"
 	"gorm.io/gorm"
 )
@@ -9,28 +11,36 @@ type DiagnosticRequestRepo struct {
 	Database *gorm.DB
 }
 
+// Create creates a persistent DiagnosticRequest row in the sql datastore
+// When a given DiagnosticRequest already has a non-zero ID an error will be returned
 func (diagnosticRequestRepo *DiagnosticRequestRepo) Create(diagnosticRequest *model.DiagnosticRequest) error {
-	return nil
+	if diagnosticRequest.ID == 0 {
+		if err := diagnosticRequestRepo.Database.Create(diagnosticRequest).Error; err != nil {
+			return err
+		}
+		return nil
+	}
+	return errors.New("record already in database")
 }
 
 func (diagnosticRequestRepo *DiagnosticRequestRepo) Update(diagnosticRequest *model.DiagnosticRequest) error {
-	return nil
+	return errors.New("not implemented")
 }
 
 func (diagnosticRequestRepo *DiagnosticRequestRepo) Delete(diagnosticRequest *model.DiagnosticRequest) error {
-	return nil
+	return errors.New("not implemented")
 }
 
 func (diagnosticRequestRepo *DiagnosticRequestRepo) GetByID(diagnosticRequestID uint) (*model.DiagnosticRequest, error) {
-	return nil, nil
+	return nil, errors.New("not implemented")
 }
 
 func (diagnosticRequestRepo *DiagnosticRequestRepo) GetByVetOrgID(vetOrgID uint) ([]model.DiagnosticRequest, error) {
-	return nil, nil
+	return nil, errors.New("not implemented")
 }
 
 func (diagnosticRequestRepo *DiagnosticRequestRepo) GetByUserID(userID uint) ([]model.DiagnosticRequest, error) {
-	return nil, nil
+	return nil, errors.New("not implemented")
 }
 
 func (diagnosticRequestRepo *DiagnosticRequestRepo) GetByCustomerID(customerID uint) ([]model.DiagnosticRequest, error) {
